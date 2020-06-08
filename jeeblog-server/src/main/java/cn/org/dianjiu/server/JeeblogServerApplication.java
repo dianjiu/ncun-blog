6e9c70aafaef831ecacbe9ac81b552a13a586a33
Limw <mengwei@point9.cn>
2020-06-09 00:53:10 +0800

    初始化server工程

---
+++ jeeblog-server/src/main/java/cn/org/dianjiu/server/JeeblogServerApplication.java
@@ -0,0 +1,13 @@
+package cn.org.dianjiu.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeblogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeblogServerApplication.class, args);
+    }
+
+}
